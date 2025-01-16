7e5cf1e76e974ae3a331273e21f5dbb582317b1f
Megisa Tabaku <tabakumegisa@gmail.com>
2025-01-16 18:26:08 +0100

    Krijojme klasen UserAccount

--- InxhinieriSofti/src/inxhinierisofti/UserAccount.java
+++ InxhinieriSofti/src/inxhinierisofti/UserAccount.java
@@ -1,11 +1,8 @@
 package inxhinierisofti;
-
 import java.util.HashMap;
 import java.util.Map;
-
 public class UserAccount {
     private Map<String, String> users = new HashMap<>();
-
     // Method to create a new account
     public boolean createAccount(String username, String password) {
         if (users.containsKey(username)) {
@@ -14,7 +11,6 @@ public class UserAccount {
         users.put(username, password);
         return true; // Account created successfully
     }
-
     // Method to validate user login
     public boolean login(String username, String password) {
         return users.getOrDefault(username, "").equals(password);
